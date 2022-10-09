ddea74db558d67bbbfe9ac48f24b2484975557bb
Constantinos Papadopoulos <14839869+cnpapado@users.noreply.github.com>
2022-10-10 00:18:19 +0300

    Add example programs and gen

---
+++ misc/syntax_gen/gen.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Generates N Edsger programs in the 'progs' dir.
+#   (N is a command-line argument.)
+#
+# To use it, you need to first declare the dirs
+# where Erlang/OTP and PropEr are in your system.
+
+ERL=/home/kostis/HiPE/otp/bin/erl
+export ERL_LIBS=/home/kostis/HiPE/proper
+
+${ERL}c -pa ${ERL_LIBS}/ebin +debug_info *.erl
+
+mkdir -p progs
+for i in $(seq 1 $1)
+do
+  $ERL -noshell -noinput -s edsger_pp p -s erlang halt > progs/p$i.eds
+done
