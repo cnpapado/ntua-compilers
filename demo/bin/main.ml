009bebb03830f6415ded25b42cde8b928bc0ce24
Constantinos Papadopoulos <14839869+cnpapado@users.noreply.github.com>
2023-11-17 12:00:08 +0200

    Make AST variants with public constructors using modules
    - Need to find a way to eliminate redundant code by
      using a parameterized module/functor

--- demo/bin/main.ml
+++ demo/bin/main.ml
@@ -1,2 +1,15 @@
-open Demo
-let () = print_endline (Ast.AST_parse_info.f 1)
+open Demo.Ast
+(* let () = print_endline (Ast.AST.f (Ast.AST.B(true))) *)
+let get_str1 e = 
+  match e with 
+  | P_info_AST.Int({node=z; meta=_}) -> string_of_int z
+  | P_info_AST.Bool({node=z; meta=_}) -> if z then "true" else "false"
+
+let get_str2 e = 
+  match e with 
+  | AST.Int(z) -> string_of_int z
+  | AST.Bool(z) -> if z then "true" else "false"
+
+let () = 
+  print_endline 
+    ((get_str1 (P_info_AST.Int({node=1; meta=234354;}))) ^ "\n" ^ (get_str2 (AST.Bool(true))))
--- demo/lib/ast.ml
+++ demo/lib/ast.ml
@@ -1,8 +1,63 @@
-module type AST = sig 
-  val f : int -> string
+(* module type AST_types = sig 
+  type expr = I of int | B of bool
+  val f : expr -> string
+end *)
+
+(* module AST = struct
+  type expr = I of int | B of bool
+  let f x = 
+    match x with 
+    | I(y) -> "int " ^ string_of_int y
+    | B(y) -> "bool " ^ if y then "true" else "false"
 end
 
-module AST_parse_info : AST = struct
-  let f x = "hi" ^ string_of_int x
+
+module ParseST = struct 
+  include AST
+  type 'a PST_t = {node: 'a; metadata: int} 
+end *)
+
+
+(* module AST : sig 
+  include module type of AST_types
+  val make_expr : int -> expr 
+end = struct 
+  type expr = I of int
+  and binop = Plus of int*int | Minus of int*int
+
+  let f x = match x with I(x) -> "hi" ^ string_of_int x
+  let make_expr x = I(x) 
+end *)
+
+
+(* type metadata = int
+
+module AST_parse_info : AST_types = struct
+
+  type expr = {node: AST.expr; meta: metadata}
+  and binop = {line_no: int; char_no: int}  
+
+  let f x = "bye" ^ string_of_int x
+
+end *)
+
+
+type 'a parse_info_node = {node: 'a; meta: int} 
+module P_info_AST = struct 
+  type expr = Bool of bool parse_info_node | Int of int parse_info_node
 end
 
+type 'a ast_node = 'a
+module AST = struct 
+  type expr = Bool of bool ast_node | Int of int ast_node
+end
+
+
+(* module A = struct 
+  type 'a node
+  type expr = Bool of bool node
+end
+
+module B = struct
+  type node = bool
+end *)
