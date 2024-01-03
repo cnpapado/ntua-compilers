a3c1e36bac2fd1a291e5e09ecc2223b989a3bc26
Constantinos Papadopoulos <14839869+cnpapado@users.noreply.github.com>
2024-01-03 15:37:43 +0200

    Impl single pass of llift's step 2

--- src/llift/llift.mli
+++ src/llift/llift.mli
@@ -3,4 +3,4 @@ open Ast
 (* module SS = Set.Make(String) *)
 
 val rename_ast : SemAST.local_def -> SemAST.local_def 
-val replace_free : SemAST.local_def -> string list
+val replace_free : SemAST.local_def -> SemAST.local_def
