eed1d5997a131153f5a17e390eeee2afb8dbcb8c
Constantinos Papadopoulos <14839869+cnpapado@users.noreply.github.com>
2024-04-10 19:33:50 +0300

    added more tests from gs-suite

--- programs/test_suite/tests/run_tests.sh
+++ programs/test_suite/tests/run_tests.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
-GRC=./../grace_compiler/gracec.exe
 
-for FILE in /home/c/Documents/Sxolh/Compilers/ntua-compilers/programs/test_suite/tests/scope/*;
+for FILE in tests/parser/*;
 do 
     f="$(basename -- $FILE)"
     echo "Running $f"
     if !([[ $f == err* ]]); then
-    # Testcase should not produce an error
-        if (! $GRC $FILE) &> /dev/null
+        # Testcase should not produce an error
+        if (! ./gracec $FILE) &> /dev/null
             then echo "$f failed: Error not expected but happened"
         fi
-    elif ($GRC $FILE) &> /dev/null
     # Testcase should produce an error
+    elif (./gracec $FILE) &> /dev/null
         then echo "$f failed: Error expected but did not happen"
     fi
+    # else echo "Success"
 done
